3cd8e63b1571bc0b7dc6616145c66bda06304940
Piotr Zieliński <piotrziel@outlook.com>
2024-02-19 00:37:50 +0100

    SPI PCLK function implementation

--- Drivers/Src/stmf446xx_spi_driver.c
+++ Drivers/Src/stmf446xx_spi_driver.c
@@ -1,7 +1,27 @@
 #include "stmf446xx_spi_driver.h"
 
 void SPI_PeriClockControl(SPI_RegDef_t *pSPIx, uint8_t enable) {
-
+    if (enable) {
+        if (pSPIx == SPI1) {
+            SPI1_PCLK_EN();
+        } else if (pSPIx == SPI2) {
+            SPI2_PCLK_EN();
+        } else if (pSPIx == SPI3) {
+            SPI3_PCLK_EN();
+        } else if (pSPIx == SPI4) {
+            SPI4_PCLK_EN();
+        }
+    } else {
+        if (pSPIx == SPI1) {
+            SPI1_PCLK_DI();
+        } else if (pSPIx == SPI2) {
+            SPI2_PCLK_DI();
+        } else if (pSPIx == SPI3) {
+            SPI3_PCLK_DI();
+        } else if (pSPIx == SPI4) {
+            SPI4_PCLK_DI();
+        }
+    }
 }
 
 void SPI_Init(SPI_Handle_t *pSPIHandle) {
